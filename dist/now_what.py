0698ed31599cbb0dabd73d39fe5aa1e3e5ec8753
Бойко Алексей Борисович <boyko_ab@gmc.local>
2020-04-08 20:43:04 +0300

    now reading list of PC from file

--- dist/now_what.py
+++ dist/now_what.py
@@ -2,52 +2,73 @@ import os
 import pythonping
 import socket
 
-
-user = 'gmc\\' + input('введите пользователя ')
-pasw = input('введите пароль ')
-ARM = input('введите имя компьютера для получения информации ')
-print('********************')
-# если АРМ доступен, узнаём IP адрес
-response = pythonping.ping(ARM, count=1 )
-if response.success():
-    IP_address = socket.gethostbyname(ARM)
-    print('IP адрес АРМ-а: ' + IP_address)
-    #узнаём версию СПО
-    os.system('wmic /NODE:\"' + ARM + '\" /USER:\"' + user + '\" /password: \"' + pasw + '\" product get name| findstr \"Дежурн\" > temp.txt')
-    if os.stat('temp.txt').st_size == 0:
+def knowversion(ARM,way_to): #берем версию программы установленной сейчас
+    os.system('wmic /NODE:\"' + ARM + '\" /USER:\"' + user + '\" /password: \"' + pasw + '\" product get name| findstr \"' + name_programm + '\">\"' + way_to + '\\temp\\\"' + ARM + '.txt')
+    if os.stat(way_to + '\\temp\\' + ARM + '.txt').st_size == 0:
         print('Программы не найдено')
         tmp = ''
-    with open('temp.txt') as t:
+    with open(way_to + '\\temp\\' + ARM + '.txt') as t:
         for tmp in t:
             tmp = tmp.encode('cp1251').decode('cp866').strip()
-            break
-    print('СПО: ' + tmp)
-    #узнаём активного пользователя
-    os.system('wmic.exe /node:\"' + ARM + '\" computersystem get username > temp.txt')
-    with open('temp.txt') as user_tmp:
+            return tmp
+            #break
+    #return tmp
+
+def knowuser(ARM, way_to):
+    os.system('wmic.exe /node:\"' + ARM + '\" computersystem get username >' + way_to + '\\temp\\' + ARM + '.txt')
+    with open(way_to + '\\temp\\' + ARM + '.txt') as user_tmp:
         linecount = 0
         for tmp in user_tmp:
             tmp = tmp.strip()
             linecount += 1
             if linecount == 3:
-                user = tmp
-                print('Сейчас на АРМ-е работает пользователь: ' + user)
-    #узнаём версию OS
-    os.system('WMIC /NODE:\"' + ARM + '\" OS get Caption > temp.txt')
-    with open('temp.txt') as version_tmp:
+                return tmp
+
+def know_OS(ARM,way_to):
+    os.system('WMIC /NODE:\"' + ARM + '\" OS get Caption >' + way_to + '\\temp\\' + ARM + '.txt')
+    with open(way_to + '\\temp\\' + ARM + '.txt') as version_tmp:
         linecount = 0
         for tmp in version_tmp:
             tmp = tmp.encode('cp1251').decode('cp866').strip()
             linecount += 1
             if linecount == 3:
-                version = tmp
-                print('Версия ОС: ' + version)
+                return tmp
 
+name_programm = 'Дежурн' #поиск программы ведется по этому словосочетанию
+user = 'gmc\\' + input('type username ')
+pasw = input('type password ')
+way_to = 'c:\\nowwhat'  #папка где всё хранится
 
-else:
-    print(ARM + ' is not available')
+with open(way_to + "\list.txt") as list_of_arms:
+    for ARM in list_of_arms:
+        ARM = ARM.strip()
+        print('********************')
+
+        # если АРМ доступен, узнаём IP адрес
+        response = pythonping.ping(ARM, count=1 )
+        if response.success():
+            IP_address = socket.gethostbyname(ARM)
+            print('IP addres of ARM: ' + IP_address)
+            print('Name of ARM ' + ARM)
+
+            #узнаём версию СПО
+            print('searching soft...')
+            tmpprogramm = knowversion(ARM,way_to)
+            if tmpprogramm != '':
+                print('now installed ' + tmpprogramm)
+            else:
+                print('No DT programm installed')
+
+            #узнаём активного пользователя
+            user_DT = knowuser(ARM,way_to)
+            print('Now working: ' + user_DT + ' user')
+
+            #узнаём версию OS
+            version = know_OS(ARM,way_to)
+            print('Версия ОС: ' + version)
+
+        else:
+            print(ARM + ' is not available')
 
 while True:
     pass
-
-
