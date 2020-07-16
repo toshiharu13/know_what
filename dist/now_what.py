c9ef1db080e0dec55ff1111341c4a93093fb892b
toshiharu13 <52210358+toshiharu13@users.noreply.github.com>
2020-07-16 12:44:42 +0300

    Add files via upload

--- dist/now_what.py
+++ dist/now_what.py
@@ -39,6 +39,18 @@ def logwritting(data):
     with open(way_to + '\\log.txt', 'a')as logfile:
         logfile.write(data + '\n')
 
+def if_ARM_online(arm):
+    try:
+        response = pythonping.ping(arm, count=1)
+        if response.success():
+            print(arm + ' доступен')
+        else:
+            print(arm + ' не доступен')
+            return False
+    except:
+        print(arm + ' не доступен')
+        return False
+
 def Start(ivent):
     # передаём занчение логин пароль в переменные
     global user, pasw
@@ -51,47 +63,43 @@ def Start(ivent):
             ARM = ARM.strip()
             print(linebreake)
             logwritting(linebreake)
+            logwritting(ARM)
 
             # если АРМ доступен, узнаём IP адрес
-            response = pythonping.ping(ARM, count=1)
-            try:
-                if response.success():
-                    IP_address = socket.gethostbyname(ARM)
-                    print('IP addres of ARM: ' + IP_address)
-                    print('Name of ARM ' + ARM)
-                    logwritting(ARM)
-                    logwritting(IP_address)
+            if if_ARM_online(ARM) == False:
+                print("не доступен")
+                logwritting("не доступен")
+                continue
+            else:
+                IP_address = socket.gethostbyname(ARM)
+                print('Name of ARM ' + ARM)
+                print('IP addres of ARM: ' + IP_address)
 
-                    # узнаём версию СПО
-                    print('searching soft...')
-                    tmpprogramm = knowversion(ARM, way_to)
-                    if tmpprogramm != '':
-                        print('now installed ' + tmpprogramm)
-                    else:
-                        print('No DT programm installed')
-                    logwritting(tmpprogramm)
+                logwritting(IP_address)
 
-                    # узнаём активного пользователя
-                    user_DT = knowuser(ARM, way_to)
-                    print('Now working: ' + user_DT + ' user')
-                    logwritting(user_DT)
-
-                    # узнаём версию OS
-                    version = know_OS(ARM, way_to)
-                    print('Версия ОС: ' + version)
-                    logwritting(version)
-
+                # узнаём версию СПО
+                print('searching soft...')
+                tmpprogramm = knowversion(ARM, way_to)
+                if tmpprogramm != '':
+                    print('now installed ' + tmpprogramm)
                 else:
-                    notavailable = ARM + ' is not available'
-                    print(notavailable)
-                    logwritting(notavailable)
-            except:
-                print('can\'t work with' + ARM)
-                break
+                    print('No DT programm installed')
+                logwritting(tmpprogramm)
+
+                # узнаём активного пользователя
+                user_DT = knowuser(ARM, way_to)
+                print('Now working: ' + user_DT + ' user')
+                logwritting(user_DT)
+
+                # узнаём версию OS
+                version = know_OS(ARM, way_to)
+                print('Версия ОС: ' + version)
+                logwritting(version)
+
+
     print(linebreake)
     print(linebreake)
-    # while True:
-    #   pass
+
 
 
 
@@ -105,6 +113,12 @@ way_to = os.path.dirname(way_to)
 with open(way_to + '\\log.txt', 'w') as logfile:
     logfile.write(str(datetime.datetime.now()) + '\n')
 
+# читаем файл с армами для отображения в окне
+with open(way_to + "\list.txt") as list_of_arms:
+    text_of_arms = list_of_arms.readlines()
+    text_of_arms = "".join(text_of_arms)
+
+
 #Основное окно программы
 main_window = tkinter.Tk()
 main_window.title("Know_what программа анализа сведений об АРМ-е")
@@ -115,22 +129,22 @@ list_arm_window.title("Список АРМ-ов")
 list_arm_window.geometry('300x250+1110+200')
 #описание элементов окна
 lbl_log = tkinter.Label(main_window, text = "файл лога: " + way_to + "\log.txt", font = ("Arial", 8, 'bold'))
-lbl_list = tkinter.Label(main_window, text = "список запрашиваемых АРМ-ов: " + way_to + "\list.txt", font = ("Arial", 8, 'bold'))
+lbl_list = tkinter.Label(list_arm_window, text = "список АРМ-ов: " + way_to + "\list.txt", font = ("Arial", 8, 'bold'))
 enter_passw = tkinter.Entry(main_window, width = 20, show = "*")
 enter_login = tkinter.Entry(main_window, width = 20)
 lbl_login = tkinter.Label(main_window, text = "User")
 lbl_passw = tkinter.Label(main_window, text = "Password")
 btn_start = tkinter.Button(main_window, text = "Start")
-lbl_list_arm = tkinter.Label(list_arm_window, text = "List of ARMS")
+lbl_list_arm = tkinter.Label(list_arm_window, text = text_of_arms)
 # расстановка элементов в окне
 lbl_log.place(x = 5, y = 310)
-lbl_list.place(x = 5, y = 330)
+lbl_list.place(x = 5, y = 5)
 enter_login.place(x = 80, y = 60)
 enter_passw.place(x = 350, y = 60)
 lbl_login.place(x = 45, y = 60)
 lbl_passw.place(x = 285, y = 60)
 btn_start.place(x =260, y = 120)
-lbl_list_arm.place(x = 5, y = 5)
+lbl_list_arm.place(x = 5, y = 20)
 
 # кнопка старта
 btn_start.bind('<Button-1>', Start)
