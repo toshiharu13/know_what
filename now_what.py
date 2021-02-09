01418642fa1b5bd22241ea41ca2c52feed8e29b6
toshiharu13 <52210358+toshiharu13@users.noreply.github.com>
2021-02-09 12:51:49 +0300

    Add files via upload

--- now_what.py
+++ now_what.py
@@ -5,19 +5,20 @@ import datetime
 import tkinter
 
 def knowversion(ARM,way_to): #берем версию программы установленной сейчас
-    os.system('wmic /NODE:\"' + ARM + '\" /USER:\"' + user + '\" /password: \"' + pasw + '\" product get name| findstr \"' + name_programm + '\">\"' + way_to + '\\temp\\\"' + ARM + '.txt')
-    if os.stat(way_to + '\\temp\\' + ARM + '.txt').st_size == 0:
+    os.system(f'wmic /NODE:\"{ARM}\" /USER:\"{user}\" /password: \"{pasw}\" product get name| findstr \"{name_programm}\">\"{way_to}\\temp\\\"{ARM}.txt')
+    if os.stat(f'{way_to}\\temp\\{ARM }.txt').st_size == 0:
         print('Программы не найдено')
         tmp = ''
         return tmp
-    with open(way_to + '\\temp\\' + ARM + '.txt') as t:
+    with open(f'{way_to}\\temp\\{ARM}.txt') as t:
         for tmp in t:
             tmp = tmp.encode('cp1251').decode('cp866').strip()
             return tmp
 
 def knowuser(ARM, way_to):
-    os.system('wmic.exe /node:\"' + ARM + '\" computersystem get username >' + way_to + '\\temp\\' + ARM + '.txt')
-    with open(way_to + '\\temp\\' + ARM + '.txt') as user_tmp:
+    #os.system('wmic.exe /node:\"' + ARM + '\" computersystem get username >' + way_to + '\\temp\\' + ARM + '.txt')
+    os.system(f'wmic.exe /node:\"{ARM}\" computersystem get username >{way_to}\\temp\\{ARM}.txt')
+    with open(f'{way_to}\\temp\\{ARM}.txt') as user_tmp:
         linecount = 0
         for tmp in user_tmp:
             tmp = tmp.strip()
@@ -26,8 +27,9 @@ def knowuser(ARM, way_to):
                 return tmp
 
 def know_OS(ARM,way_to):
-    os.system('WMIC /NODE:\"' + ARM + '\" OS get Caption >' + way_to + '\\temp\\' + ARM + '.txt')
-    with open(way_to + '\\temp\\' + ARM + '.txt') as version_tmp:
+    #os.system('WMIC /NODE:\"' + ARM + '\" OS get Caption >' + way_to + '\\temp\\' + ARM + '.txt')
+    os.system(f'WMIC /NODE:\"{ARM}\" OS get Caption >{way_to}\\temp\\{ARM}.txt')
+    with open(f'{way_to}\\temp\\{ARM}.txt') as version_tmp:
         linecount = 0
         for tmp in version_tmp:
             tmp = tmp.encode('cp1251').decode('cp866').strip()
@@ -36,7 +38,7 @@ def know_OS(ARM,way_to):
                 return tmp
 
 def logwritting(data):
-    with open(way_to + '\\log.txt', 'a')as logfile:
+    with open(f'{way_to}\\log.txt', 'a')as logfile:
         logfile.write(data + '\n')
 
 def if_ARM_online(arm):
@@ -63,13 +65,13 @@ def makdir_files():
 def Start(ivent):
     # передаём занчение логин пароль в переменные
     global user, pasw
-    user = 'gmc\\' + enter_login.get()  # имя пользователя с правами админа
+    user = f'gmc\\{enter_login.get()}'  # имя пользователя с правами админа
     pasw = enter_passw.get()  # пароль
 
     makdir_files() # проверяем темп папку, если нет, создаём
 
     # читаем по списку армы
-    with open(way_to + "\list.txt") as list_of_arms:
+    with open(f'{way_to}\\list.txt') as list_of_arms:
         for ARM in list_of_arms:
             ARM = ARM.strip()
             print(linebreake)
@@ -118,11 +120,11 @@ way_to = os.path.abspath(__file__)
 way_to = os.path.dirname(way_to)
 
 # подгатавливаем файл логов к записи событий данной сессии
-with open(way_to + '\\log.txt', 'w') as logfile:
+with open(f'{way_to}\\log.txt', 'w') as logfile:
     logfile.write(str(datetime.datetime.now()) + '\n')
 
 # читаем файл с армами для отображения в окне
-with open(way_to + "\list.txt") as list_of_arms:
+with open(f'{way_to}\\list.txt') as list_of_arms:
     text_of_arms = list_of_arms.readlines()
     text_of_arms = "".join(text_of_arms)
 
@@ -136,8 +138,8 @@ list_arm_window = tkinter.Tk()
 list_arm_window.title("Список АРМ-ов")
 list_arm_window.geometry('300x250+1110+200')
 #описание элементов окна
-lbl_log = tkinter.Label(main_window, text = "файл лога: " + way_to + "\log.txt", font = ("Arial", 8, 'bold'))
-lbl_list = tkinter.Label(list_arm_window, text = "список АРМ-ов: " + way_to + "\list.txt", font = ("Arial", 8, 'bold'))
+lbl_log = tkinter.Label(main_window, text = f'файл лога: {way_to}\\log.txt', font = ("Arial", 8, 'bold'))
+lbl_list = tkinter.Label(list_arm_window, text =f'список АРМ-ов: {way_to}\\list.txt', font = ("Arial", 8, 'bold'))
 enter_passw = tkinter.Entry(main_window, width = 20, show = "*")
 enter_login = tkinter.Entry(main_window, width = 20)
 lbl_login = tkinter.Label(main_window, text = "User")
